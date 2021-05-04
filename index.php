b5134aa8c64697766f9d8ad8f3062c1840d23530
Jacopo Vinanti <jacopovinanti@gmail.com>
2021-05-04 17:02:47 +0200

    Ritocchi

--- index.php
+++ index.php
@@ -52,6 +52,9 @@ $faqs = [
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="stylesheet" href="css/style.css">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;700&display=swap" rel="stylesheet">
 <title>Google faqs</title>
 </head>
 <body>
