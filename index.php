0a8ddab51db635622bb47111aaf6f26fa4987447
Jacopo Vinanti <jacopovinanti@gmail.com>
2021-05-04 16:25:04 +0200

    Aggiunta ordered list

--- index.php
+++ index.php
@@ -24,21 +24,23 @@ $faqs = [
         "<a href='#'>Scopri</a> quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a te il controllo.",
       ]
   ],
-  // [
-  //   'h2' => "Perché il mio account è associato a un paese?",
-  //   'p' => [
-  //       "Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:",
-  //       'ol' => [
-  //         "La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:",
-  //         'nestedOl' => [
-  //           "Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.",
-  //           "Google LLC, con sede negli Stati Uniti, per il resto del mondo."
-  //         ],
-  //         "La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.",
-  //       ],
-  //       "<a href='#'>Scopri</a> quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a te il controllo.",
-  //     ]
-  // ],
+  [
+    'h2' => "Perché il mio account è associato a un paese?",
+    'p' => [
+        "Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:",
+        [
+          'ol' => [
+            "La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:",
+            // 'nestedOl' => [
+            //   "Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.",
+            //   "Google LLC, con sede negli Stati Uniti, per il resto del mondo."
+            // ],
+            "La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.",
+          ],
+        ],
+        "<a href='#'>Scopri</a> quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a te il controllo.",
+      ]
+  ],
 ]
 ?>
 
@@ -48,7 +50,7 @@ $faqs = [
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<title>Snacks</title>
+<title>Google faqs</title>
 </head>
 <body>
   <main>
@@ -57,11 +59,17 @@ $faqs = [
         foreach ( $faqs[$i] as $key => $value) {
           if ( $key == 'h2') {
             echo '<h2>'.$value.'</h2>';
-          }
-          if ( $key == 'p' ) {
+          } else if ( $key == 'p' ) {
             for ( $j = 0; $j < count( $value ); $j++ ) {
-              echo '<p>'.$value[$j].'</p>';
-
+              if ( is_array($value[$j]) && array_key_exists( 'ol', $value[$j] ) ) {
+                  echo '<ol>';
+                  for ( $y = 0; $y < count( $value[$j]['ol'] ); $y++ ) {
+                    echo '<li>'.$value[$j]['ol'][$y].'</li>';
+                  }
+                  echo '</ol>';
+              } else {
+                  echo '<p>'.$value[$j].'</p>';
+              }
             }
           }
         }
